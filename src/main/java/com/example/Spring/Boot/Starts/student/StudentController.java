d33074f27c116c8b2116bf900867be7976398440
justyniaczek <justynareplin@gmail.com>
2021-06-21 15:04:13 +0200

    ADD StudentConfig

--- src/main/java/com/example/Spring/Boot/Starts/student/StudentController.java
+++ src/main/java/com/example/Spring/Boot/Starts/student/StudentController.java
@@ -1,9 +1,8 @@
 package com.example.Spring.Boot.Starts.student;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 import java.time.LocalDate;
 import java.util.List;
 
@@ -19,6 +18,23 @@ public class StudentController {
     @GetMapping
     public List<Student> getStudents(){
         return studentService.getStudents();
+    }
+
+    @PostMapping
+    public void registerNewStudent(@RequestBody Student student){
+        studentService.addNewStudent(student);
+    }
+    @DeleteMapping(path="{studentId}")
+    public void deleteStudent(@PathVariable("studentId") Long StudentId){
+        studentService.deleteStudent(StudentId);
+    }
+
+    @PutMapping(path="{studentId}")
+    public void updateStudent(
+            @PathVariable("studentId") Long studentId,
+            @RequestParam(required = false) String name,
+            @RequestParam(required = false) String email){
+        studentService.updateStudent(studentId, name, email);
     }
 
 }
--- src/main/java/com/example/Spring/Boot/Starts/student/StudentService.java
+++ src/main/java/com/example/Spring/Boot/Starts/student/StudentService.java
@@ -5,8 +5,11 @@ import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.transaction.Transactional;
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 
 @Service
 public class StudentService {
@@ -20,6 +23,42 @@ public class StudentService {
     public List<Student> getStudents(){
         return studentRepository.findAll();
     }
+    public void addNewStudent(Student student) {
+        Optional<Student> studentOptional = studentRepository
+                .findStudentByEmail(student.getEmail());
+        if (studentOptional.isPresent()){
+            throw new IllegalStateException("email taken");
+        }
+        studentRepository.save(student);
+        System.out.println(student);
+    }
 
-
+    public void deleteStudent(Long studentId) {
+        boolean exists = studentRepository.existsById(studentId);
+            if(!exists){
+                throw new IllegalStateException(
+                        "student with id" + studentId +"does not exists");
+            }
+            studentRepository.deleteById(studentId);
+    }
+    @Transactional
+    public void updateStudent(Long studentId,
+                              String name,
+                              String email){
+        Student student = studentRepository.findById(studentId)
+                .orElseThrow(() -> new IllegalStateException(
+                        "student with id" + studentId + "doesnt exist"));
+        if(name != null &&
+                name.length() > 0 && !Objects.equals(student.getName(), name)){
+            student.setName(name);
+        }
+        if(email != null &&
+                email.length() > 0 && !Objects.equals(student.getEmail(), email)){
+            Optional<Student> studentOptional = studentRepository
+                    .findStudentByEmail(email);
+            if(studentOptional.isPresent()){     //checking if email is already n databa se
+                throw new IllegalStateException("email taken");
+            }student.setEmail(email);
+        }
+    }
 }
